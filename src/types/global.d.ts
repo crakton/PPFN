24d9900296514f199a02d7ecbedcc5a10ebc3e80
Jrcity <redemptionjonathan1@gmail.com>
2023-02-14 22:16:21 +0100

    app tunings
    include new packages to upload images to firestorage
    type definitions for env files

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,5 +1,8 @@
 declare module "@env" {
 	export const agora_appid: string;
 	export const agora_tokenuri: string;
+	export const email_api_key: string;
+	export const fcm_server: string;
 	export const kommunicate_appid: string;
+	export const sudo_email_apikey: string;
 }
