0201aa185acadfcb27a9735d4e0b7eb42d5d1aac
Jrcity <redemptionjonathan1@gmail.com>
2023-02-14 22:08:28 +0100

    Extending calender and it props

--- src/components/CustomCalendar.tsx
+++ src/components/CustomCalendar.tsx
@@ -1,67 +1,27 @@
-import React, {useCallback, useEffect, useMemo, useState} from "react";
-import {Calendar} from "react-native-calendars";
+import React from "react";
+import {Calendar, DateData} from "react-native-calendars";
 import FoIcon from "react-native-vector-icons/Fontisto";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import {Text, TouchableOpacity} from "react-native";
+import {Text} from "react-native";
 import {formatMonth} from "../utils/formatMonth";
 import {style} from "../constants/style";
-import {Toast} from "react-native-awesome";
 
-export const CustomCalendar = React.memo(function () {
-	const [bookDates, setBookDates] = useState(new Set(""));
-	const handleSelectedDates = useCallback(
-		({
-			dateString,
-			day,
-			month,
-		}: {
-			dateStrng: string;
-			day: string;
-			month: string;
-		}) => {
-			if (
-				day >= new Date().getDate() &&
-				month >= new Date().getMonth() + 1
-			) {
-				const newBookDates = new Set(bookDates);
-
-				if (newBookDates.has(dateString)) {
-					newBookDates.delete(dateString);
-				} else {
-					newBookDates.add(dateString);
-				}
-				setBookDates(newBookDates);
-			} else {
-				Toast.showToast({
-					message: "You can't set appointment in the past!",
-					type: "waring",
-					duration: 2000,
-				});
-			}
-		},
-		[bookDates],
-	);
-
-	const markedDates = useMemo(
-		() =>
-			[...bookDates].reduce((obj, item) => {
-				obj[item] = {
-					selected: true,
-					marked: true,
-					selectedColor: "#FE593D",
-				};
-				return obj;
-			}, {}),
-		[bookDates],
-	);
-
+export const CustomCalendar = React.memo(function ({
+	myCustomCalendar = false,
+	markers,
+	handleDateChange,
+}: {
+	myCustomCalendar?: boolean;
+	markers: any;
+	handleDateChange: (date: DateData) => void;
+}) {
 	return (
 		<Calendar
-			onDayPress={handleSelectedDates}
+			hideArrows={myCustomCalendar ? false : true}
+			onDayPress={handleDateChange}
 			headerStyle={{
 				backgroundColor: style.primaryColor,
 			}}
-			markedDates={markedDates}
+			markedDates={markers}
 			renderHeader={date => (
 				<Text
 					style={{
@@ -72,24 +32,28 @@ export const CustomCalendar = React.memo(function () {
 					{formatMonth(date)}
 				</Text>
 			)}
-			renderArrow={direction => (
-				<>
-					{direction === "left" && (
-						<FoIcon
-							color={style.titleColor}
-							size={18}
-							name="angle-left"
-						/>
-					)}
-					{direction === "right" && (
-						<FoIcon
-							color={style.titleColor}
-							size={18}
-							name="angle-right"
-						/>
-					)}
-				</>
-			)}
+			renderArrow={
+				myCustomCalendar
+					? direction => (
+							<>
+								{direction === "left" && (
+									<FoIcon
+										color={style.titleColor}
+										size={18}
+										name="angle-left"
+									/>
+								)}
+								{direction === "right" && (
+									<FoIcon
+										color={style.titleColor}
+										size={18}
+										name="angle-right"
+									/>
+								)}
+							</>
+					  )
+					: undefined
+			}
 		/>
 	);
 });
