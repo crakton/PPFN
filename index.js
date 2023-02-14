b76f2022ed181688ff4c36cd948aba589f934bc1
Jrcity <redemptionjonathan1@gmail.com>
2023-02-14 20:47:49 +0100

    headless js

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 /**
  * @format
  */
-import React from 'react';
-import {AppRegistry} from 'react-native';
-import {Provider} from 'react-redux';
-import App from './App';
-import {name as appName} from './app.json';
-import {store} from './src/redux/store';
+import React from "react";
+import {AppRegistry} from "react-native";
+import {Provider} from "react-redux";
+import App from "./App";
+import {name as appName} from "./app.json";
+import {store} from "./src/redux/store";
+import firebaseServices from "./src/services/firebase.service";
 
-const Main = () => (
-	<Provider store={store}>
-		<App />
-	</Provider>
-);
+// Register background handler
+firebaseServices.backgroundFCM();
 
-AppRegistry.registerComponent(appName, () => Main);
+const HeadlessCheck = ({isHeadless}) => {
+	if (isHeadless) {
+		return null;
+	}
+	return (
+		<Provider store={store}>
+			<App />
+		</Provider>
+	);
+};
+
+AppRegistry.registerComponent(appName, () => HeadlessCheck);
